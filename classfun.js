9cc629ce77ce8ac6b4ff4dbddf76abb353d987a8
Ayush kumar <ayushkumar2006@gmail.com>
2026-09-01 11:56:58 +0530

    classfun

---
+++ classfun.js
@@ -0,0 +1,19 @@
+
+        class student{
+            constructor(name,pincode){
+                this.name=name;
+                this.pincode=pincode;
+            }
+
+        
+        display(){
+            console.log(this.name);
+            console.log(this.pincode);
+
+        }
+    }
+    let s1=new student("Ayush",654);
+      let s2=new student("Ayush",654);
+      console.log(s1);
+    console.log(s2);
+    
