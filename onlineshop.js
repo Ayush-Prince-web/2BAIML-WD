85d6a2bb3492e80aeec35a6fee8b4d9b6ca6a2b6
Ayush kumar <ayushkumar2006@gmail.com>
2026-09-01 11:44:45 +0530

    onlineshop

---
+++ onlineshop.js
@@ -0,0 +1,17 @@
+
+        let product={
+            ID:52,
+            Name: "toy",
+            price: 65,
+            Quantity: 2,
+            totalpr: function(){
+         this.tp= (price*quantity);
+          }
+         
+        }
+//   product.Quantity(4);
+console.log(product.ID);
+console.log(product.Name);
+console.log(product.tp);
+console.log(product.Quantity);
+ 
