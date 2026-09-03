5b10adb15ba75fb1fb3efc0cf0c6ed4019839908
Ayush kumar <ayushkumar2006@gmail.com>
2026-09-03 11:06:57 +0530

    atringtemp

---
+++ stringtemp.js
@@ -0,0 +1,10 @@
+let firstname="Ayush"
+let lastname="kuam"
+console.log("my firstname is "+firstname)
+console.log("my lastname is "+lastname)
+console.log( `full name ${firstname}${lastname}`)
+console.log(`my firstname is $ {firstname} $ {last name }`);
+
+
+
+
